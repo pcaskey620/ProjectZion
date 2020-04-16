2ff1bb464257936cb3f86359785ac23ca3787ece
Parker <parker.caskey@gmail.com>
2020-04-16 13:53:49 -0600

    added pager buttons, removed auto sliding

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 var TIMEOUT = 8000;
  
-var interval = setInterval(handleNext, TIMEOUT);
+//var interval = setInterval(handleNext, TIMEOUT);
  
 function handleNext() {
- 
   var $radios = $('input[class*="slide-radio"]');
   var $activeRadio = $('input[class*="slide-radio"]:checked');
  
@@ -13,18 +12,39 @@ function handleNext() {
   $radios
     .attr('checked', false);
  
-  if (currentIndex > radiosLength - 1) {
- 
+  if (currentIndex > radiosLength - 1) { 
     $radios
       .first()
-      .attr('checked', true);
- 
-  } else {
- 
+      .attr('checked', true); 
+  } else { 
     $activeRadio
       .next('input[class*="slide-radio"]')
       .attr('checked', true);
- 
   }
- 
 }
+
+function handlePrev() {
+    var $radios = $('input[class*="slide-radio"]');
+    var $activeRadio = $('input[class*="slide-radio"]:checked');
+   
+    var currentIndex = $activeRadio.index();
+    var radiosLength = $radios.length;
+   
+    $radios
+      .attr('checked', false);
+   
+    if (currentIndex > radiosLength - 1) { 
+      $radios
+        .first()
+        .attr('checked', true); 
+    } else { 
+      $activeRadio
+        .prev('input[class*="slide-radio"]')
+        .attr('checked', true);
+    }
+}
+
+$(document).ready(function() {
+    $('.pager-right').click(handleNext);
+    $('.pager-left').click(handlePrev);
+});
